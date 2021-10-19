e900ca8b9d0ae04108f48520f48a31da96fe1a54
Ina <inastatkic@gmail.com>
2021-10-19 13:13:44 +0200

    Initial Commit

---
+++ Sources/GoogleMobileAds/GoogleMobileAds.swift
@@ -0,0 +1,6 @@
+public struct GoogleMobileAds {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/GoogleMobileAdsTests/GoogleMobileAdsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GoogleMobileAds
+
+final class GoogleMobileAdsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GoogleMobileAds().text, "Hello, World!")
+    }
+}
